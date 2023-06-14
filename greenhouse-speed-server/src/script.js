899f5dfb9f6ef78ccc8ee79474f601c4b1d5ef3a
parksnrec <parker.goldsberry@gmail.com>
2023-06-14 13:01:11 -0600

    save point

--- greenhouse-speed-server/src/script.js
+++ greenhouse-speed-server/src/script.js
@@ -1,3 +1,36 @@
-function getOffTime() {
+function onSeconds() {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+      
+    fetch("/data/on_seconds", requestOptions)
+        .then(response => response.text())
+        .then(result => document.getElementById("ontime").innerHTML = result)
+        .catch(error => console.log('error', error));
+}
+
+function offSeconds() {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+      
+    fetch("/data/off_seconds", requestOptions)
+        .then(response => response.text())
+        .then(result => document.getElementById("offtime").innerHTML = result)
+        .catch(error => console.log('error', error));
+}
+
+function setOnSeconds() {
     
 }
+
+function setOffSeconds() {
+
+}
+
+
+// run on page load
+onSeconds();
+offSeconds();
